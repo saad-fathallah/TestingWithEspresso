ecfec6b8530b9c8df06d05b51a61a6cfdbede6ec
saad <saadfathallah420@gmail.com>
2023-04-04 16:13:26 +0000

    Espresso_tp

---
+++ app/src/main/java/com/example/testingwithespresso/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.testingwithespresso;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView viewById = (TextView) findViewById(R.id.resultView);
+        Bundle inputData = getIntent().getExtras();
+        String input = inputData.getString("input");
+        viewById.setText(input);
+}
+}
